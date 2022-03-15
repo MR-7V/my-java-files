38fbf34c047eef39665baa0f87f001eba827dc2b
mohith-venkata <mohithreddy.venkata@gmail.com>
2022-03-15 09:05:57 +0530

    testing

---
+++ dynamic programming/Most6FibonacciNum.java
@@ -0,0 +1,23 @@
+// import java.util.Arrays;
+
+// public class Most6FibonacciNum {
+//     public static void main(String args[]){
+//         System.out.println(Fib(25));
+
+//     }
+
+//     static int[] Fib(int n){
+//         int temp=0;
+//         int fib[]=new int[n];
+//         Arrays.fill(fib,-1);
+//         int i=0;
+//         fib[0]=0;
+//         fib[1]=1;
+//         while(temp<=n){
+//             temp=fib[i]+fib[i+1];
+//             fib[i]=temp;
+//             i++;
+//         }
+//         return fib;
+//     }
+// }
